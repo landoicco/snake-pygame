f5c98a4ed4a9a12d7dd4b7aaf88283af5c5854a6
Lando Icaza <landoicco@gmail.com>
2021-11-30 19:51:59 -0600

    Fruit class defined and working!

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 import sys
-
 import pygame
+import random
+from pygame.math import Vector2
 
 pygame.init()
-screen = pygame.display.set_mode((400, 500))
+cell_size = 40
+cell_number = 20
+screen = pygame.display.set_mode((cell_number * cell_size, cell_number * cell_size))
 clock = pygame.time.Clock()
+
+
+class FRUIT:
+    def __init__(self):
+        self.x = random.randint(0, cell_number - 1)
+        self.y = random.randint(0, cell_number - 1)
+        self.pos = Vector2(self.x, self.y)
+
+    def draw_fruit(self):
+        fruit_rect = pygame.Rect(int(self.pos.x * cell_size),
+                                 int(self.pos.y * cell_size), cell_size, cell_size)
+
+        pygame.draw.rect(screen, pygame.color.Color("red"), fruit_rect)
+
+
+fruit = FRUIT()
 
 while True:
     for event in pygame.event.get():
@@ -12,5 +31,7 @@ while True:
             pygame.quit()
             sys.exit()
 
+    screen.fill(pygame.Color('green'))
+    fruit.draw_fruit()
     pygame.display.update()
     clock.tick(60)
